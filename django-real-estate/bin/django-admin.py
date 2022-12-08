0d5542497d87290e1918114c9c59dd5de7c192ed
sainimkar <nimkarsai@gmail.com>
2022-12-08 12:11:33 -0800

    initial commit

---
+++ django-real-estate/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/sainimkar/273 projects git/real-estate/django-real-estate/django-real-estate/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
